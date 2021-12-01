e1c3d62cbc12cb65896ec5f6d61ffc4e36d47fd8
Thomas Bittner <thomas.bittner@posteo.de>
2021-12-01 17:43:21 +0100

    Set options for character export's save file dialog

--- client/wwwroot/filesystem.js
+++ client/wwwroot/filesystem.js
@@ -1,5 +1,5 @@
 async function saveAs(options, content) {
-	var fileHandle = await window.showSaveFilePicker(options);
+	var fileHandle = await window.showSaveFilePicker(JSON.parse(options));
 	const writable = await fileHandle.createWritable();
 	await writable.write(content);
 	await writable.close();
---
+++ systems/Base/CharacterSerialization.cs
@@ -0,0 +1,25 @@
+﻿namespace Dorc.RoleplayingSystems.Base
+{
+	using Microsoft.Extensions.Logging;
+	using Microsoft.JSInterop;
+	using Newtonsoft.Json;
+	using System;
+
+	public class CharacterSerialization
+	{
+		public static async void ExportCharacter(Character character, IJSRuntime js, ILogger<Index> logger)
+		{
+			try
+			{
+				var characterJson = JsonConvert.SerializeObject(character, Formatting.Indented);
+				await js.InvokeVoidAsync("saveAs",
+						$"{{\"suggestedName\": \"{character?.Name}\", \"types\": [{{ \"description\": \"D'Orc characters\", \"accept\": {{\"dorc/character\": [\".char\"]}}}}]}}",
+						characterJson);
+			}
+			catch (Exception exception)
+			{
+				logger.LogError($"Could not export character: {exception.Message}");
+			}
+		}
+	}
+}
