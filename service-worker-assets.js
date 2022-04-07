98246aef1861ddf891d972fb58ea149d9cb06195
gadse <peter-bozsoky@posteo.de>
2022-04-07 10:09:31 +0000

    Deploying to gh-pages from  @ b886013e8076b84b0bebcc105a98de1639630312 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -53,7 +53,7 @@
       "url": "_framework\/icudt.dat"
     },
     {
-      "hash": "sha256-KdWhtx+R8DUJKF14v3LeTK+rftCUOwxq4KtS3WWGXyc=",
+      "hash": "sha256-WQSPtc1359Dn7HucEmWM3176+vcMQyXxPGmY1B04D64=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -69,7 +69,7 @@
       "url": "_framework\/Characters.View.dll"
     },
     {
-      "hash": "sha256-pDnNGt9KQcgcSPFu0TNw4UoMjdsDOFEHBhoWJ4FU0ns=",
+      "hash": "sha256-iOWO+yl4sIWXeCq\/EskrrTferA9dU2VjpZwvB2Mby+k=",
       "url": "_framework\/CoreWarrior.dll"
     },
     {
@@ -77,7 +77,7 @@
       "url": "_framework\/dice.dll"
     },
     {
-      "hash": "sha256-MmVS+ZP1g9pKc5itRFZ0nFA5ULDb\/YRoN2e6Gd66OSk=",
+      "hash": "sha256-59clTYWzB4teZJVnnoAqmbhtdTf5z9Dh7Y+U6dPUfUc=",
       "url": "_framework\/Dorc.Client.dll"
     },
     {
@@ -405,5 +405,5 @@
       "url": "_content\/Characters.View\/Characters.View.bundle.scp.css"
     }
   ],
-  "version": "PbFhGUQN"
+  "version": "CEs61zoa"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: PbFhGUQN */
+/* Manifest version: CEs61zoa */
